c1eabf36ba30fe56930627a0d371f6851fb2a7d6
Retno Winarsih <ret.winars@gmail.com>
2022-10-24 11:00:54 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Retno";
+$umur = 22;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Imam";
+$umurAdik = 17;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun"
+
+?>
